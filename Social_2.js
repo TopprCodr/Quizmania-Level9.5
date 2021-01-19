76a8d14a50209188bdb294936dee3ece270fff36
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-01-19 15:31:59 +0530

    Update Social_2.js

--- Social_2.js
+++ Social_2.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import SnackBar from "../components/SnackBar";
-
+import firebase from '../FirebaseConfig';
 export default function Social({ navigation }) {
     const [usersQuizes, setUsersQuizes] = useState({});
     const [users, setUsers] = useState([]);
