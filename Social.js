2e9e20879651a319d9f16b904134c8dd2d6eda92
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-01-19 15:32:25 +0530

    Update Social.js

--- Social.js
+++ Social.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import SnackBar from "../components/SnackBar";
+import firebase from '../FirebaseConfig';
 
 export default function Social({ navigation }) {
     const [usersQuizes, setUsersQuizes] = useState({});
